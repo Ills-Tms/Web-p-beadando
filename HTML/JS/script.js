29e5fb8fcfcee221b37fed0d75fda5e63098c610
ITD <a@a.a>
2026-01-15 13:42:04 +0100

    ASAS

--- HTML/JS/script.js
+++ HTML/JS/script.js
@@ -2,3 +2,18 @@ const form =document.getElementById("regform");
 const fnev=document.getElementById("fnev");
 const email=document.getElementById("email");
 const jelsz=document.getElementById("jelszo");
+const btn=document.getElementById("");
+
+form.addEventListener("submit" , function (event) {event.preventDefault()
+
+    let username = localStorage.setItem("fnev",fnev)
+    let useremail = localStorage.setItem("email",email)
+    let userpass = localStorage.setItem("password",jelsz)
+
+
+
+
+
+    window.location.href="login.html";
+
+ } )
