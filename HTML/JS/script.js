5662c26a32439205dd3e4f07ee373f6dc6ea7874
ITD <a@a.a>
2026-01-15 13:01:33 +0100

    pages and sum

---
+++ HTML/JS/script.js
@@ -0,0 +1,4 @@
+const form =document.getElementById("regform");
+const fnev=document.getElementById("fnev");
+const email=document.getElementById("email");
+const jelsz=document.getElementById("jelszo");
